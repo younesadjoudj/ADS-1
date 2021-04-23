3af82f453e10c632de90e9eb6fbe472c466e8563
younesadjoudj <79196405+younesadjoudj@users.noreply.github.com>
2021-04-23 17:51:33 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,21 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+        int num = 0, left = 0, right = size, mid;
+        while (left < right) {
+                mid = left + (right - left) / 2;
+                if (arr[mid] == value) {
+                        while (arr[mid-1] == value) mid--;
+                        while (arr[mid] == value) mid++, num++;
+                        return num;
+                }else if (arr[mid] > value)
+                } else if (arr[mid] > value) {
+                        right = mid;
+                else
+                } else {
+                        left = mid+1;
+                }
+        }
+
+        return 0; 
 }
